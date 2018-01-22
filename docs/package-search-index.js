10e14217823132fd53e69da404a18f6dd9e2cdb0
DesertCookie <roov4.email@gmail.com>
2018-01-22 15:49:31 +0100

    initial push of javadoc of version 0.5.0
    Took 2 minutes

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"desertcookie.simple2d"},{"l":"desertcookie.simple2d.core"},{"l":"desertcookie.simple2d.entities"},{"l":"desertcookie.simple2d.exceptions"},{"l":"desertcookie.simple2d.input"},{"l":"desertcookie.simple2d.rendering"},{"l":"desertcookie.simple2d.sound"},{"l":"desertcookie.simple2d.textures"},{"l":"desertcookie.simple2d.ui"}]
